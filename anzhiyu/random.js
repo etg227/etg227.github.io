9c2bb49809815f7c72f14aba9d6d98ed5c186622
etg227 <aikyan227@gmail.com>
2026-05-29 18:51:03 +0800

    Site updated: 2026-05-29 18:51:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/14/hello-world/","2025/04/20/post/","2025/04/20/test/"];function toRandomPost(){
+var posts=["2025/04/20/post/","2025/04/20/test/","2025/01/14/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
